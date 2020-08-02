d84afa2916e43df168757370c00b542314f2e695
ChuinHongYap <57805747+ChuinHongYap@users.noreply.github.com>
2020-08-02 16:22:38 +0800

    Create simple_model.py

---
+++ MNIST/simple_model.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+
+'''
+MNIST example using simple model
+'''
+
+mnist = tf.keras.datasets.mnist
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+INPUT_SHAPE = train_images.shape[1:]
+BATCH_SIZE = 32
+EPOCHS = 5
+
+# preprocessing
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+def simple_model():
+  input = tf.keras.layers.Input(shape=(INPUT_SHAPE+(1,))) #for gray scale image
+  net = tf.keras.layers.Flatten()(input)  #flatten data
+  net = tf.keras.layers.Dense(200)(net)
+  net = tf.keras.layers.Dense(200)(net)
+
+  output = tf.keras.layers.Dense(10, activation='sigmoid')(net)
+
+  return tf.keras.Model(input,output)
+
+model = simple_model()
+model.summary() #show model
+
+model.compile(optimizer='adam', loss= 'sparse_categorical_crossentropy',metrics=['accuracy'])
+model.fit(train_images, train_labels, batch_size=BATCH_SIZE, epochs=EPOCHS
+          , validation_data=(test_images, test_labels)
+          )
