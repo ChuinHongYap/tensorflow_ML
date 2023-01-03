58363d92767aabb48e089623d12dc26c0bd56b5f
ChuinHongYap <57805747+ChuinHongYap@users.noreply.github.com>
2023-01-03 11:44:41 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 import shutil
 from glob import glob
+import os
 
 # move all files to a single directory by getting all files recursively
 def move_files(input_path, output_path, file_ext=".jpg"):
   all_files = sorted(glob(input_path+"*/*"+file_ext, recursive = True))
   for files in all_files:
-    shutil.move(files, OUT_PATH+files.split('/')[-1])
+    shutil.move(files, os.path.join(output_path, files.split('/')[-1]))
   
